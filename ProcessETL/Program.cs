24280c72fa280d51fa54f14318abfa0ac4e7ec91
Victor Paixão <vpma_amorim@hotmail.com>
2019-03-23 11:57:11 -0300

    Adicionando projeto ao GIT

---
+++ ProcessETL/Program.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using System;
+using System.Data;
+using System.IO;
+
+namespace ProcessETL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program programa = new Program();
+
+                DataTable tabelaProduto = programa.PopularTabelaArtistas();
+            }
+            catch (BadDataException bdEx)
+            {
+                Console.WriteLine(bdEx);
+            }
+        }
+
+        private DataTable PopularTabelaArtistas()
+        {
+            DataTable tabelaArtistas = new DataTable();
+
+            using (FileStream file = File.Open(@"C:\Users\1161166413\Desktop\artistas.csv", FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new StreamReader(file))
+            {
+                using (var csv = new CsvReader(reader))
+                {
+                    while (csv.Read())
+                    {
+                        var name = csv.GetField<string>(0);
+                        tabelaArtistas.Rows.Add(name);
+                    }
+                }
+
+                return tabelaArtistas;
+            }
+        }
+    }
+}
